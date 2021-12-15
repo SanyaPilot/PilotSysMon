71f6da5830b64caa60983c7f65a272a94f1e7745
Alexander Baransky <alexander.baranskiy@yandex.ru>
2021-12-15 17:58:37 +0300

    SysInfo: optimize cards rendering

--- src/components/SysInfo.vue
+++ src/components/SysInfo.vue
@@ -1,88 +1,21 @@
 <template>
   <v-row>
-    <v-col cols=12 md=4>
+    <v-col cols=12 md=4 v-for="(card, name) in cards" :key="name">
       <v-card hover>
         <v-card-title class="title-img">
-          OS
+          {{card.label}}
           <v-img
-            :src="getOSIcon"
+            v-if="card.iconType == 'svg'"
+            :src="card.icon == 'os' ? getOSIcon : card.icon"
             contain
             max-height="120"
             max-width="120"
           ></v-img>
+          <v-icon v-else size=120> {{card.icon == 'network' ? getNetworkIcon : card.icon}} </v-icon>
         </v-card-title>
         <v-card-text>
           <table>
-            <tr v-for="item in os" :key="item.name">
-              <td><b>{{item.name}}</b></td>
-              <td class="table-value">{{item.value}}</td>
-            </tr>
-          </table>
-        </v-card-text>
-      </v-card>
-    </v-col>
-    <v-col cols=12 md=4>
-      <v-card hover>
-        <v-card-title class="title-img">
-          Hardware
-          <v-icon size=120> {{icons.mdiServer}} </v-icon>
-        </v-card-title>
-        <v-card-text>
-          <table>
-            <tr v-for="item in hardware" :key="item.name">
-              <td><b>{{item.name}}</b></td>
-              <td class="table-value">{{item.value}}</td>
-            </tr>
-          </table>
-        </v-card-text>
-      </v-card>
-    </v-col>
-    <v-col cols=12 md=4>
-      <v-card hover>
-        <v-card-title class="title-img">
-          Network
-          <v-icon size=120> {{getNetworkIcon}} </v-icon>
-        </v-card-title>
-        <v-card-text>
-          <table>
-            <tr v-for="item in network" :key="item.name">
-              <td><b>{{item.name}}</b></td>
-              <td class="table-value">{{item.value}}</td>
-            </tr>
-          </table>
-        </v-card-text>
-      </v-card>
-    </v-col>
-    <v-col cols=12 md=4>
-      <v-card hover>
-        <v-card-title class="title-img">
-          Time
-          <v-icon size=120> {{icons.mdiClockOutline}} </v-icon>
-        </v-card-title>
-        <v-card-text>
-          <table>
-            <tr v-for="item in time" :key="item.name">
-              <td><b>{{item.name}}</b></td>
-              <td class="table-value">{{item.value}}</td>
-            </tr>
-          </table>
-        </v-card-text>
-      </v-card>
-    </v-col>
-    <v-col cols=12 md=4>
-      <v-card hover>
-        <v-card-title class="title-img">
-          Python
-          <v-img
-            :src="require('../assets/python.svg')"
-            contain
-            max-height="120"
-            max-width="120"
-          ></v-img>
-        </v-card-title>
-        <v-card-text>
-          <table>
-            <tr v-for="item in python" :key="item.name">
+            <tr v-for="item in card.data" :key="item.name">
               <td><b>{{item.name}}</b></td>
               <td class="table-value">{{item.value}}</td>
             </tr>
@@ -100,47 +33,42 @@ export default {
   name: 'SysInfo',
 
   data: () => ({
-    icons: {
-      mdiWifiStrength3,
-      mdiLanPending,
-      mdiClockOutline,
-      mdiServer
+    cards: {
+      os: {label: "OS", iconType: "svg", icon: "os", data: [
+        {name: "Family:", value: "Linux"},
+        {name: "Name:", value: "Manjaro"},
+        {name: "Version:", value: "12.1"},
+        {name: "Kernel:", value: "5.15.5"},
+      ]},
+      hardware: {label: "Hardware", iconType: "icon", icon: mdiServer, data: [
+        {name: "CPU:", value: "AMD Ryzen 5 5600X (12) @ 3.700GHz"},
+        {name: "GPU:", value: "AMD ATI Radeon 540/540X/550/550X / RX 540X/550/550X"},
+        {name: "Memory:", value: "16010 MiB"},
+      ]},
+      network: {label: "Network", iconType: "icon", icon: "network", data: [
+        {name: "Type:", value: "WiFi"},
+        {name: "Hostname:", value: "PiHomeServer"},
+        {name: "IPv4:", value: "192.168.1.3"},
+        {name: "IPv6:", value: "ладно, не могу придумать"},
+      ]},
+      time: {label: "Time", iconType: "icon", icon: mdiClockOutline, data: [
+        {name: "Time on server:", value: "12:57"},
+        {name: "Server timezone:", value: "Europe/Moscow"},
+        {name: "Uptime:", value: "2 days, 08:43:22"},
+      ]},
+      python: {label: "Python", iconType: "svg", icon: require('../assets/python.svg'), data: [
+        {name: "Version:", value: "3.10.1"},
+        {name: "PIP Version:", value: "2.14.2"},
+        {name: "Venv:", value: "Not used"},
+      ]},
     },
-    os: [
-      {name: "Family:", value: "Linux"},
-      {name: "Name:", value: "Manjaro"},
-      {name: "Version:", value: "12.1"},
-      {name: "Kernel:", value: "5.15.5"},
-    ],
-    hardware: [
-      {name: "CPU:", value: "AMD Ryzen 5 5600X (12) @ 3.700GHz"},
-      {name: "GPU:", value: "AMD ATI Radeon 540/540X/550/550X / RX 540X/550/550X"},
-      {name: "Memory:", value: "16010 MiB"},
-    ],
-    network: [
-      {name: "Type:", value: "WiFi"},
-      {name: "Hostname:", value: "PiHomeServer"},
-      {name: "IPv4:", value: "192.168.1.3"},
-      {name: "IPv6:", value: "ладно, не могу придумать"},
-    ],
-    time: [
-      {name: "Time on server:", value: "12:57"},
-      {name: "Server timezone:", value: "Europe/Moscow"},
-      {name: "Uptime:", value: "2 days, 08:43:22"},
-    ],
-    python: [
-      {name: "Version:", value: "3.10.1"},
-      {name: "PIP Version:", value: "2.14.2"},
-      {name: "Venv:", value: "Not used"},
-    ],
   }),
-
   computed: {
     getNetworkIcon: function() {
-      return this.network[0].value == 'Ethernet' ? this.icons.mdiLanPending : this.icons.mdiWifiStrength3
+      return this.cards.network.data[0].value == 'Ethernet' ? mdiLanPending : mdiWifiStrength3
     },
     getOSIcon: function() {
-      switch (this.os[0].value) {
+      switch (this.cards.os.data[0].value) {
         case 'Linux':
           return require('../assets/linux.svg')
         case 'Windows':
